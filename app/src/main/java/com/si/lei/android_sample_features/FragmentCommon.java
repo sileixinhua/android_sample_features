658ad772794391bab03dc0596b7d5b94b658002f
sileixinhua <sileixinhua@163.com>
2019-02-15 15:48:44 +0800

    【Task】【DJI安卓工具箱】底部导航栏完成

---
+++ app/src/main/java/com/si/lei/android_sample_features/FragmentCommon.java
@@ -0,0 +1,36 @@
+package com.si.lei.android_sample_features;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author lei.si
+ * @date 2019/02/15
+ * 添加底部导航栏
+ * https://github.com/yaochangliang159/Android-TabView
+ */
+public class FragmentCommon extends Fragment {
+    TextView textView;
+
+    public static FragmentCommon newInstance(String text){
+        FragmentCommon fragmentCommon=new FragmentCommon();
+        Bundle bundle=new Bundle();
+        bundle.putString("text",text);
+        fragmentCommon.setArguments(bundle);
+        return fragmentCommon;
+    }
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_common,container,false);
+        textView= (TextView) view.findViewById(R.id.textView);
+        textView.setText(getArguments().getString("text"));
+        return view;
+    }
+}
--- app/src/main/java/com/si/lei/android_sample_features/MainActivity.java
+++ app/src/main/java/com/si/lei/android_sample_features/MainActivity.java
@@ -2,12 +2,24 @@ package com.si.lei.android_sample_features;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
+/**
+ * @author lei.si
+ * @date 2019/02/15
+ * 添加底部导航栏
+ * https://github.com/yaochangliang159/Android-TabView
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void quick_start(View view){
+        startActivity(new Intent(this,QuickStartActivity.class));
     }
 }
