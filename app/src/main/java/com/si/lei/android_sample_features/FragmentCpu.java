9a4d6a021b46ec2057c061d8cb8ac776ad16f5d6
sileixinhua <sileixinhua@163.com>
2019-02-20 13:28:16 +0800

    【Task】【DJI安卓工具箱】导航栏 内存 基础页面 实时更新

--- app/src/main/java/com/si/lei/android_sample_features/FragmentCpu.java
+++ app/src/main/java/com/si/lei/android_sample_features/FragmentCpu.java
@@ -1,12 +1,17 @@
 package com.si.lei.android_sample_features;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author lei.si
@@ -34,6 +39,36 @@ public class FragmentCpu extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cpu, container, false);
         textView = (TextView) view.findViewById(R.id.textView);
+        new TimeThread().start();
         return view;
     }
+
+    public class TimeThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            do{
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler.sendMessage(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while (true);
+
+        }
+    }
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    textView.setText(new SimpleDateFormat("HH:mm:ss").format(new Date(System.currentTimeMillis())));
+                    break;
+            }
+            return false;
+        }
+    });
 }
